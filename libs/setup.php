562248ff8eee3af51f968f077125002ca39165c7
Eduardo Breitkopf <101363313+Edub96@users.noreply.github.com>
2023-02-16 12:59:39 +0100

    Update setup.php

--- libs/setup.php
+++ libs/setup.php
@@ -8,8 +8,8 @@ $config->set('modelsFolder', 'models/');
 $config->set('viewsFolder', 'views/');
 
 // Parámetros de conexión a la BD
-$config->set('dbhost', 'localhost');
-$config->set('dbname', 'dwespruebas');
-$config->set('dbuser', 'super');
-$config->set('dbpass', '123456');
+$config->set('dbhost', 'containers-us-west-195.railway.app:6487');
+$config->set('dbname', 'railway');
+$config->set('dbuser', 'root');
+$config->set('dbpass', 'q4etx1THz0dTTKdc6dwH');
 ?>
